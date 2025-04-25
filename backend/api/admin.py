3c9b099f59d02d2cecaf014b1ec67b073d0387d3
BeastBoy <timur22090505@gmail.com>
2025-04-26 02:01:26 +0500

    ++Password encr.

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,12 +1,41 @@
 from django.contrib import admin
 from .models import User, Badge, Event, Swipe, Chat
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.utils.translation import gettext_lazy as _
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name')
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = '__all__'
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
     list_display = ('email', 'first_name', 'last_name', 'age', 'telegram_username', 'telegram_id', 'is_staff', 'is_active')
     search_fields = ('email', 'first_name', 'last_name', 'telegram_username', 'telegram_id')
     list_filter = ('is_staff', 'is_active', 'age')
-    filter_horizontal = ('badges',)
+    filter_horizontal = ('badges', 'groups', 'user_permissions')
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'age', 'telegram_username', 'telegram_id', 'photo', 'bio')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions', 'badges')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'password1', 'password2'),
+        }),
+    )
+    ordering = ('email',)
 
 @admin.register(Badge)
 class BadgeAdmin(admin.ModelAdmin):
